f5fd1316ec5b436cfb02b9bfca6f83ae476931c5
Lucas Barros <lbarros.comp@gmail.com>
2020-10-17 03:50:15 -0300

    Inserting the first section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import '../styles/Index.module.scss';
+import { 
+  Container,
+} from '../styles/pages/Home';
+import picture1 from '../assets/picture1.jpg';
 
 export default function Index() {
   return (
@@ -10,7 +14,14 @@ export default function Index() {
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
       </Head>
       <body>
-        <div className="title">LUCAS BARROS</div>
+        <Container>
+          <div className="title">
+            LUCAS<br />BARROS
+            <br />
+            <div className="subtitle">Fullstack Developer</div>
+          </div>
+          <img className="picture1" src={picture1} />
+        </Container>
       </body>
     </div>
   )
--- styles/pages/Home.js
+++ styles/pages/Home.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    position: relative;
+    display: grid;
+    grid-template-columns: 50% 50%;
     min-height: 60vh;
+    align-items: center;
 `;
