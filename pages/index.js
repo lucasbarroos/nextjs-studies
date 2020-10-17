e931b08f894d9e5dc52ee21e44166a8c8f4eb7f4
Lucas Barros <lbarros.comp@gmail.com>
2020-10-17 04:38:52 -0300

    Inserting the first sections

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,38 @@
 import Head from 'next/head'
 import '../styles/Index.module.scss';
-import { 
-  Container,
-} from '../styles/pages/Home';
 import picture1 from '../assets/picture1.jpg';
+import picture2 from '../assets/picture2.jpg';
 
 export default function Index() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Lucas Barros</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
-        <Container>
+        <div className="container">
           <div className="title">
             LUCAS<br />BARROS
             <br />
             <div className="subtitle">Fullstack Developer</div>
           </div>
           <img className="picture1" src={picture1} />
-        </Container>
+        </div>
+        <div class="about-container">
+          <div className="about-border-effect" />
+          <div className="about-section">
+              <div className="about-section-picture-content">
+                <img className="about-section-picture" src={picture2} />
+              </div>
+              <span className="about-section-text">
+                I'm a Fullstack Developer with 6 years of experience.
+                I really love studing, without excuses. Every time I’m searching for new features and ideas to increment my career. 
+                When you love you profession, you will never work.
+              </span>
+          </div>
+        </div>
       </body>
     </div>
   )
--- styles/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-    position: relative;
-    display: grid;
-    grid-template-columns: 50% 50%;
-    min-height: 60vh;
-    align-items: center;
-`;
